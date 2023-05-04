7b9b5c4f1fa6a61a8a9a89a8b8f3ea19eb76b053
thehappybuilder <66582744+thehappybuilder@users.noreply.github.com>
2023-05-04 04:25:35 -0700

    pushy pushy

--- frontend/src/pages/HitTheRoad.js
+++ frontend/src/pages/HitTheRoad.js
@@ -1,3 +1,17 @@
+import legacy from '../img/legacy-logo.png'
+import hittheroad_splash from '../img/hittheroad_main.png'
+import lineart1 from '../img/lineart1.png'
+import steamIcon from '../img/steamIcon.png'
+import youtubeIcon from '../img/youtubeIcon.png'
+import discordIcon from '../img/discordIcon.png'
+import lineart2 from '../img/lineart2.png'
+import bg_dark_2 from '../img/bg_dark_2.png'
+
+import htr_1 from '../img/roads_image.png'
+import htr_2 from '../img/interior_htr_v2.png'
+
+
+
 function HitTheRoad() {
 
   // Who are we?
@@ -8,24 +22,85 @@ function HitTheRoad() {
   //  youtube
   //  twitter
   return (
-    <div className="about">
-      <section>
-        <h3>Who are we?</h3>
-        <p>
-          Hi! We're Happy Viking Studios, a two-person
-          game development company centered in
-          Los Angeles.
-        </p>
-      </section>
-      <section>
-        <h3>How can you contact us?</h3>
-        <p>
-          Reach us by sending an <a href="mailto:support@happyvikingstudios.com">
-          email</a> or joining our <a href="https://discord.gg/8vFCHahcHG">
-          discord</a>.
-        </p>
-      </section>
-    </div>
+    <span className="home-content">
+      <div className='splash-img'>
+        <img className='hidden-bg' src={hittheroad_splash} alt="splash-legacy" />
+
+        <div className='splash-title'>
+          <h1>HIT</h1>
+          <h1>THE</h1>
+          <h1>ROAD</h1>
+        </div>
+      </div>
+
+      <div className='body3'>
+        <div className='line-main'>
+          <div className= 'hor-line'>
+            <img className= 'main-line-img' src={lineart1} alt="lineart1"/>
+          </div>
+        </div>
+
+        <div className= 'welcome-body'>
+          <h3> Welcome to Hit The Road </h3>
+
+          <div className= 'welcome-desc'>
+            <p1> Race through the forests, mountains and valleys of Hit The Road.
+            Our newest project is a low poly racing and drifting game where users
+            complete contracts to earn new gear for their character and equipment
+            for their stagecoach. The path is not an easy one, with bandits,
+            drifting around cliffs, and terrifying jumps, there will never be a dull
+            moment on the road!
+            </p1>
+          </div>
+
+          <div className= 'center-style'>
+            <p1> Concept Art for interior and tracks </p1>
+          </div>
+
+          <div className= 'pic-parent-3'>
+            <img className= 'ss-img' src={htr_1} alt="voronoi2"/>
+            <img className= 'ss-img' src={htr_2} alt="llyod"/>
+          </div>
+
+          <div className= 'center-style'>
+            <p1> More information coming soon! </p1>
+          </div>
+
+
+        </div>
+
+        <div className='line-2'>
+
+            <div className= 'hor-line-left'>
+              <img className= 'second-line-img-flip' src={lineart2} alt="lineart2"/>
+            </div>
+
+
+
+            <img className= 'steam-img' src={steamIcon} alt="steamIcon"/>
+            <img className= 'youtube-img' src={youtubeIcon} alt="youtubeIcon"/>
+            <img className= 'discord-img' src={discordIcon} alt="discordIcon"/>
+
+            <div className= 'hor-line-right'>
+              <img className= 'second-line-img' src={lineart2} alt="lineart2"/>
+            </div>
+
+
+        </div>
+
+      </div>
+
+
+      <div className='body2'>
+      </div>
+
+
+
+
+
+
+
+    </span>
   );
 }
 
